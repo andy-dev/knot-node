82e78b1795e5782379ebcde819b9e484835dcd0b
andy-dev <andres.macedo@gmail.com>
2015-04-22 17:35:09 -0700

    add example 4, using a help program in command line

--- 1.js
+++ 1.js
@@ -2,18 +2,18 @@
 
 //--- EXAMPLE 1 ---
 
-  // node provides hosting enviroment for JS that makes possible to print something on the screen
-  // takes standar input output
+// node provides hosting enviroment for JS that makes possible to print something on the screen
+// takes standar input output
 
 //--- CODE ---
 
-  // console.log("hello universe")
+// console.log("hello universe")
 
 //--- NOTES ---
 
-  // console.log is a wrapper of stdout we could also write:
-  // process.stdout.write("hello universe")
-  // but this does not add a new line
+// console.log is a wrapper of stdout we could also write:
+// process.stdout.write("hello universe")
+// but this does not add a new line
 
 
 //--- EXAMPLE 2 ---
@@ -37,11 +37,13 @@
 // minimist returns a function that we call immediately and we remove the first two parameters(node and 1js)
 // we alse tell it to take the name parameter as a string instead of a boolean
 
-var args = require("minimist")(process.argv.slice(2), {string : "name"})
+//--- CODE ---
 
-var name = args.name;
+// var args = require("minimist")(process.argv.slice(2), {string : "name"})
 
-console.log("Hello " + name);
+// var name = args.name;
+
+// console.log("Hello " + name);
 
 // Running in the command line: node 1.js #=> Hello undefined
 // Running in the command line: node 1.js --name=World #=> Hello World
@@ -52,3 +54,34 @@ console.log("Hello " + name);
 // #!/usr/bin/env node
 // you then dont need to use node
 
+//--- EXAMPLE 4 ---
+
+// With command line programs that you do not pass the right parameters it shows some sort of help
+// Will do simple function (note, there are packages that do this for us)
+
+//--- CODE ---
+
+function printHelp(){
+  console.log("1.js Node Program");
+  console.log("");
+  console.log("usage:");
+  console.log("--help                     print this help");
+  console.log("--name                     say hello to {NAME}");
+
+}
+
+var args = require("minimist")(process.argv.slice(2), {string : "name"})
+
+if (args.help || !args.name) { // if you passed in a help parameter or you did not pass args.name
+  printHelp();                 // print help and then we exit the program
+  process.exit(1);             // in the browser we would use return but we are in the global scope
+}
+
+var name = args.name;
+
+console.log("Hello " + name);
+
+// Running in the command line: node 1.js #=> Should print help function
+// Running in the command line: node 1.js --help #=> Should print also help function
+// Running in the command line: node 1.js --name2=Andy #=> Wrong parameter name, will also print help function
+// Running in the command line: node 1.js --name=Andy #=> Hello Andy
