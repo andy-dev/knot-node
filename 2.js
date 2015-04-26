6ca3a786828208d669e6c6aaeb1cfdf753069f61
andy-dev <andres.macedo@gmail.com>
2015-04-25 19:10:49 -0700

    dealing with async with nodejs

--- 2.js
+++ 2.js
@@ -4,12 +4,47 @@
 
 //--- CODE ---
 
+// function printHelp(){
+//   console.log("2.js Node Program");
+//   console.log("");
+//   console.log("usage:");
+//   console.log("--help                     print this help");
+//   console.log("--file={NAME}              read file of {NAME} and output it");
+//   // read from command line --file and file name
+
+// }
+
+// var args = require("minimist")(process.argv.slice(2), {string : "file"})
+
+// if (args.help || !args.file) {
+//   printHelp();
+//   process.exit(1);
+// }
+
+// var hello = require("./helloworld.js"); // load the module in
+                                        // we use require statement
+                                        // minimanist not path bc it was in default include location
+                                        // what is
+
+// var contents = hello.say(args.file); // looking for file, one of our parameters
+
+// console.log(contents.toString()); // if we do not pass the toString to our buffer,
+                                  // reating an array buffer binary rep of our data
+
+
+
+// Running in the command line: node 2.js --file=test.txt #=> Should print Hello World
+
+
+
+//--- CODE Using Call back ---
+
 function printHelp(){
   console.log("2.js Node Program");
   console.log("");
   console.log("usage:");
   console.log("--help                     print this help");
-  console.log("--file={NAME}              read file of {NAME} and output");
+  console.log("--file={NAME}              read file of {NAME} and output it");
 
 }
 
@@ -20,16 +55,18 @@ if (args.help || !args.file) {
   process.exit(1);
 }
 
-var hello = require("./helloworld.js"); // load the module in
-                                      // we use require statement
-                                      // minimanist not path bc it was in default include location
-
-var contents = hello.say(args.file);
-
-console.log(contents.toString());
+var hello = require("./helloworld.js");
 
 
-// Running in the command line: node 1.js #=> Should print help function
-// Running in the command line: node 1.js --help #=> Should print also help function
-// Running in the command line: node 1.js --name2=Andy #=> Wrong parameter name, will also print help function
-// Running in the command line: node 1.js --name=Andy #=> Hello Andy
+hello.say(args.file, function(err,contents){
+  if (err){
+    console.error("Error: " + err);
+  }
+  else {
+    console.log(contents.toString());
+  }
+});
+
+// in node using callbacks the first parameter is always reserved for an error
+// Running in the command line: node 2.js --file=test.txt #=> Should print Hello World
+// Running in the command line: node 2.js --file=test.txt2 #=> Should print Hello World
--- helloworld.js
+++ helloworld.js
@@ -1,14 +1,36 @@
+// this is our module, we need a function to read our files out
+// this is how you organize your nodejs code, different modules for different tasks
+// inside this file everything is assume to be private
 
-// this is our module we need a function to read our files out
-// what is the api for the module? what is the api module
-// different modules for different tasks
+// function say(filename) {
+//   return fs.readFileSync(filename);
+// }
 
-function say(filname) {
-  return contents = fs.readFileSync(filename);
+// var fs = require("fs");
+// facility for reading file, node has a std built in module called the fs module
+
+// module.exports.say = say;
+// to expose you are given module.exports and you add to that object
+
+
+// lets get in the practice of dealing with async
+
+function say(filename,cb) {
+  return fs.readFile(filename, function(err,contents){
+    if (err){
+      cb(err);
+    }
+    else {
+      setTimeout(function(){
+        cb(null,contents);
+        },1000);
+    }
+  });
 }
 
 var fs = require("fs");
 
 module.exports.say = say;
 
-
+// will add a callback that will receive the contents
+// we are reading in the file, when we get the conts back, we check if succesful
